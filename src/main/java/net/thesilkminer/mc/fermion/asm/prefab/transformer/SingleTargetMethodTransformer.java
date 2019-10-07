0b4c6a221d66184e426a0110b5c1ebb6d3fc8956
TheSilkMiner <thesilkminer@outlook.com>
2019-10-07 20:59:26 +0200

    Update licenses
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/java/net/thesilkminer/mc/fermion/asm/prefab/transformer/SingleTargetMethodTransformer.java
+++ src/main/java/net/thesilkminer/mc/fermion/asm/prefab/transformer/SingleTargetMethodTransformer.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (C) 2019  TheSilkMiner
+ *
+ * This file is part of Fermion.
+ *
+ * Fermion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fermion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Fermion.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact information:
+ * E-mail: thesilkminer <at> outlook <dot> com
+ */
+
 package net.thesilkminer.mc.fermion.asm.prefab.transformer;
 
 import com.google.common.collect.ImmutableMap;
